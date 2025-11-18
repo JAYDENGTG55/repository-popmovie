55102e6c94c6ef95726f673bbb1817b59913b1e0
JAYDENGTG55 <jaydenhalim7@gmail.com>
2025-11-19 00:26:58 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import NavBar from "./components/NavBar";
+import Logo from "./components/Logo";
+import Search from "./components/Search";
+import NumResults from "./components/NumResults";
+import { fetchMovie } from "./helpers";
+import Main from "./components/Main";
+import BoxMovies from "./components/BoxMovies";
+import MovieList from "./components/MovieList";
+import MovieDetail from "./components/MovieDetail";
+import Loading from "./components/Loading";
+
+
+function App() {
+  const [query, setQuery] = useState("oppenheimer");
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleChangeQuery = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleOnSelectedMovie = (id) => {
+    console.log(id);
+    setSelectedMovie(id);
+  };
+
+  const handleSetNullSelectedMovie = () => {
+    setSelectedMovie(null);
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const data = await fetchMovie(query);
+        setMovies(data.Search);
+        setIsLoading(false);
+      } catch (err) {
+        console.error(err);
+        setMovies([]);
+      }
+    }
+
+    if (query.length < 5) {
+      setMovies([]);
+      return;
+    }
+
+    fetchData();
+  }, [query]);
+
+  return (
+    <>
+      <NavBar>
+        <Logo />
+        <Search query={query} handleChangeQuery={handleChangeQuery} />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <BoxMovies>
+          {isLoading ?
+            <Loading/> : <MovieList movies={movies}
+                    handleOnSelectedMovie={handleOnSelectedMovie}/>}
+        </BoxMovies>
+        <BoxMovies>
+          {selectedMovie && <MovieDetail selectedMovie={selectedMovie} 
+                handleSetNullSelectedMovie={handleSetNullSelectedMovie}
+          />}
+        </BoxMovies>
+      </Main>
+    </>
+  );
+}
+
+export default App;
